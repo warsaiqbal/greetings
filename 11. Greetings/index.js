10be7c911856752216f3d87fcf7030ecd942485c
warsaiqbal <160345806+warsaiqbal@users.noreply.github.com>
2024-03-30 08:13:17 -0700

    Add files via upload

---
+++ 11. Greetings/index.js
@@ -0,0 +1,5 @@
+var names = ["Maryum", "Alishba", "Hafsa", "Meher"];
+console.log(names[0], "is my good friend");
+console.log(names[1], "is my good friend");
+console.log(names[2], "is my good friend");
+console.log(names[3], "is my good friend");
---
+++ 11. Greetings/index.ts
@@ -0,0 +1,6 @@
+let names: string[] = ["Maryum", "Alishba", "Hafsa", "Meher"];
+
+console.log(names[0], "is my good friend");
+console.log(names[1], "is my good friend");
+console.log(names[2], "is my good friend");
+console.log(names[3], "is my good friend");
